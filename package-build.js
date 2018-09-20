756df31322b5d4d4e0559795203d8c9c489e87d0
Yohan Gomez <yohan.gz@hotmail.com>
2018-09-20 14:21:16 +0530

    Update package build script to ignore dev dependency removal

--- package-build.js
+++ package-build.js
@@ -3,6 +3,5 @@ const fs = require('fs');
 const path = require('path');
 
 delete packageJson.scripts;
-delete packageJson.devDependencies;
 
 fs.writeFileSync(path.join(process.cwd(), 'dist/package.json'), JSON.stringify(packageJson, null, 2));
