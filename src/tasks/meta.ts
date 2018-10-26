6df1f8e3c0288835b1b43ff139ef7ab7f31e05c2
Yohan Gomez <yohan.gz@hotmail.com>
2018-10-26 15:11:59 +0530

    Default packer config support

--- src/tasks/meta.ts
+++ src/tasks/meta.ts
@@ -3,6 +3,64 @@ import fs from 'fs';
 import { PackerConfig } from '../model/packer-config';
 import { PackageConfig } from '../model/package-config';
 import { BabelConfig } from '../model/babel-config';
+import merge from 'lodash/merge';
+
+const defaultPackerConfig = {
+  entry: 'index.js',
+  source: 'src',
+  dist: 'dist',
+  output: {
+    amd: {
+      define: '',
+      id: ''
+    },
+    dependencyMapMode: '',
+    esnext: true,
+    es5: true,
+    minBundle: true,
+    format: 'umd',
+    imageInlineLimit: 1000000,
+    inlineStyle: false,
+    stylesDir: 'styles',
+    namespace: ''
+  },
+  compiler: {
+    buildMode: 'browser',
+    scriptPreprocessor: 'none',
+    stylePreprocessor: 'scss',
+    styleSupport: true
+  },
+  assetPaths: [
+    'src/assets'
+  ],
+  copy: [
+    'README.md',
+    'LICENSE'
+  ],
+  bundle: {
+    externals: [],
+    globals: {},
+    mapExternals: false
+  },
+  ignore: [],
+  pathReplacePatterns: [],
+  testFramework: 'jasmine',
+  watch: {
+    scriptDir: '.tmp',
+    demoDir: 'demo/watch',
+    helperDir: 'demo/helper',
+    open: true,
+    port: 4000,
+    serve: true
+  },
+  license: {
+    banner: true,
+    thirdParty: {
+      fileName: 'dependencies.txt',
+      includePrivate: false
+    }
+  }
+};
 
 export const readConfig = (): PackerConfig => {
   return require(path.join(process.cwd(), '.packerrc.json'));
@@ -17,7 +75,8 @@ export const readCLIPackageData = (): PackageConfig => {
 };
 
 export const readBabelConfig = (esVersion: string): BabelConfig => {
-  return require(path.join(process.cwd(), `.babelrc.${esVersion}.js`));
+  const projectConfig = require(path.join(process.cwd(), `.babelrc.${esVersion}.js`));
+  return merge({}, defaultPackerConfig, projectConfig);
 };
 
 export const readBannerTemplate = (): string => {
