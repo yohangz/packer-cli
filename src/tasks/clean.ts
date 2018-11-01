9c613ca7457b08172c63692e73f55b71e0dd4f6c
Yohan Gomez <yohan.gz@hotmail.com>
2018-11-01 21:55:13 +0530

    Integrate logger to clean tasks

--- src/tasks/clean.ts
+++ src/tasks/clean.ts
@@ -3,32 +3,39 @@ import clean from 'gulp-clean';
 import path from 'path';
 
 import { readConfig } from './meta';
+import logger from '../common/logger';
 
-gulp.task('tmp:clean', () => {
+gulp.task('tmp:clean', async () => {
   try {
+    logger.trace('[tmp:clean] start');
     const config = readConfig();
     return gulp.src([path.join(process.cwd(), config.tmp)], {
-      allowEmpty: true,
-      read: false
-    })
-      .pipe(clean());
+        allowEmpty: true,
+        read: false
+      })
+      .pipe(clean())
+      .on('end', () => {
+        logger.trace('[tmp:clean] end');
+      });
   } catch (e) {
-    console.error(e);
-    throw Error('task failure');
+    logger.error('[tmp:clean] task failure:\n', e.stack || e.message);
   }
 });
 
-gulp.task('build:clean', () => {
+gulp.task('build:clean', async () => {
   try {
+    logger.trace('[build:clean] start');
     const config = readConfig();
     return gulp.src([path.join(process.cwd(), config.dist)], {
-      allowEmpty: true,
-      read: false
-    })
-      .pipe(clean());
+        allowEmpty: true,
+        read: false
+      })
+      .pipe(clean())
+      .on('end', () => {
+        logger.trace('[build:clean] end');
+      });
   } catch (e) {
-    console.error(e);
-    throw Error('task failure');
+    logger.error('[tmp:clean] task failure:\n', e.message, e.stack);
   }
 });
 
