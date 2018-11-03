767d89c5c0fc7d42e9298c528a8097be24acf610
Yohan Gomez <yohan.gz@hotmail.com>
2018-11-03 18:30:41 +0530

    Util log level based logging support

--- src/tasks/util.ts
+++ src/tasks/util.ts
@@ -1,18 +1,22 @@
 import { spawn, SpawnOptions, StdioOptions } from 'child_process';
 import fs from 'fs';
 import path from 'path';
+import { Logger } from '../common/logger';
 
 const isWindows = process.platform === 'win32';
 
 export const args = process.argv.splice(2);
 
-export const runShellCommand = (command: string, inputArguments: string[], dir: string) => {
+export const runShellCommand = (command: string, inputArguments: string[], dir: string, log: Logger) => {
   const cmd = isWindows ? `${command}.cmd` : command;
   const options: SpawnOptions = {
     cwd: dir,
     detached: false,
     stdio: 'inherit' as StdioOptions
   };
+
+  log.trace('shell command execution options:\n%o', options);
+  log.trace('shell command: %s %s', cmd, inputArguments.join(' '));
 
   return new Promise((resolve: () => void): void => {
     const childProcess = spawn(cmd, inputArguments, options);
