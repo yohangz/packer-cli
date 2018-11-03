fd0e7928beef7da1a220d46a40f9c5aa662fcecd
Yohan Gomez <yohan.gz@hotmail.com>
2018-11-03 18:30:18 +0530

    Entry log level based logging support

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,10 @@ import { args } from './tasks/util';
 import logger from './common/logger';
 
 import karmaPackerPlugin from './plugins/karma-packer-plugin';
-import { readSummary } from './tasks/meta';
+import { isValidProject, readSummary } from './tasks/meta';
 import chalk from 'chalk';
+
+const log = logger.create('');
 
 switch (args[0]) {
   case '--help':
@@ -30,19 +32,25 @@ switch (args[0]) {
     });
     break;
   case 'build':
-    gulp.series('build')(() => {
-      // no implementation
-    });
+    if (isValidProject(log)) {
+      gulp.series('build')(() => {
+        // no implementation
+      });
+    }
     break;
   case 'watch':
-    gulp.series('watch')(() => {
-      // no implementation
-    });
+    if (isValidProject(log)) {
+      gulp.series('watch')(() => {
+        // no implementation
+      });
+    }
     break;
   case 'test':
-    gulp.series('test')(() => {
-      // no implementation
-    });
+    if (isValidProject(log)) {
+      gulp.series('test')(() => {
+        // no implementation
+      });
+    }
     break;
   case 'clean':
     gulp.series('clean')(() => {
@@ -50,25 +58,27 @@ switch (args[0]) {
     });
     break;
   case 'lint': {
-    if (args.includes('--style')) {
-      gulp.series('lint:style')(() => {
-        // no implementation
-      });
+    if (isValidProject(log)) {
+      if (args.includes('--style')) {
+        gulp.series('lint:style')(() => {
+          // no implementation
+        });
+        break;
+      } else if (args.includes('--script')) {
+        gulp.series('lint:script')(() => {
+          // no implementation
+        });
+        break;
+      } else {
+        gulp.series('lint')(() => {
+          // no implementation
+        });
+      }
       break;
-    } else if (args.includes('--script')) {
-      gulp.series('lint:script')(() => {
-        // no implementation
-      });
-      break;
-    } else {
-      gulp.series('lint')(() => {
-        // no implementation
-      });
     }
-    break;
   }
   default:
-    logger.error('Invalid task name argument\n%s%s', chalk.reset('try '), chalk.blue('packer --help'));
+    log.error('Invalid task name argument\n%s%s', chalk.reset('try '), chalk.blue('packer --help'));
     break;
 }
 
