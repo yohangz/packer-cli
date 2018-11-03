0263e0ce91722b16c418236fd604904c12fd1f98
Yohan Gomez <yohan.gz@hotmail.com>
2018-11-03 18:32:37 +0530

    Version task log level based logging support

--- src/tasks/version.ts
+++ src/tasks/version.ts
@@ -1,9 +1,16 @@
 import gulp from 'gulp';
 import chalk from 'chalk';
-import { readCLIPackageData } from './meta';
 
-gulp.task('version', (done: () => void) => {
-  const config = readCLIPackageData();
-  console.log(chalk.blue(`${config.name} version:`), chalk.red(config.version));
-  done();
+import { readCLIPackageData } from './meta';
+import logger from '../common/logger';
+
+gulp.task('version', async (done: () => void) => {
+  const log = logger.create('[version]');
+  try {
+    const config = readCLIPackageData();
+    console.log(chalk.blue('%s version: %s'), config.name, chalk.red(config.version));
+    done();
+  } catch (e) {
+    log.error('task failure:\n', e.stack || e.message);
+  }
 });
