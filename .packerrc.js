8c0d6a0c72097723e3f3a35c20afb5defe428b8c
Yohan Gomez <yohan.gz@hotmail.com>
2018-11-14 19:06:38 +0530

    Upgrade packer self dependency to latest version

---
+++ .packerrc.js
@@ -0,0 +1,362 @@
+/**
+ * Packer base configuration object.
+ */
+module.exports = {
+  /**
+   * Entry source file.
+   * @type {string}
+   * @default 'index.js'
+   */
+  entry: 'index.ts',
+
+  /**
+   * Source directory
+   * @type {string}
+   * @default 'src'
+   */
+  source: 'src',
+
+  /**
+   * Build artifact output directory
+   * @type {string}
+   * @default 'dist'
+   */
+  dist: 'dist',
+
+  /**
+   * Watch and build temporary file directory
+   * @type {string}
+   * @default '.tmp'
+   */
+  tmp: '.tmp',
+
+  /**
+   * Packer compiler options
+   */
+  compiler: {
+
+    /**
+     * Dependency map mode option
+     * - 'cross-map-peer-dependency' : Map project dependencies to target peerDependencies
+     * - 'cross-map-dependency' : Map project peerDependencies to target dependencies
+     * - 'map-dependency' : Map project dependencies to target dependencies
+     * - 'map-peer-dependency' : Map project peer dependencies to target peerDependencies
+     * - 'all' - Map both peerDependencies and dependencies to target peerDependencies and dependencies
+     * @type {string}
+     * @default 'cross-map-peer-dependency'
+     */
+    dependencyMapMode: 'map-dependency',
+
+    /**
+     * If true, a separate sourcemap file will be created. If inline, the sourcemap will be appended to
+     * the resulting output file as a data URI.
+     * @type {boolean|string}
+     * @default true
+     */
+    sourceMap: true,
+
+    /**
+     * Compile build target config.
+     * @type {{}}
+     * @default {}
+     */
+    build: {
+
+      /**
+       * Generate flat bundle minified build artifact.
+       * @type {boolean}
+       * @default true
+       */
+      bundleMin: true,
+
+      /**
+       * Generate flat es5 build artifact.
+       * @type {boolean}
+       * @default false
+       */
+      es5: false,
+
+      /**
+       * Generate flat es5 minified build artifact based on .babelrc.es5.js.
+       * @type {boolean}
+       * @default false
+       */
+      es5Min: false,
+
+      /**
+       * Generate flat esnext build artifact based on .babelrc.esnext.js.
+       * @type {boolean}
+       * @default true
+       */
+      esnext: false,
+
+      /**
+       * Generate flat esnext minified build artifact.
+       * @type {boolean}
+       * @default true
+       */
+      esnextMin: false
+    },
+
+    /**
+     * Library compile mode.
+     * - 'browser' : Browser/NodeJS compliant module.
+     * - 'node' : NodeJS only module.
+     * - 'node-cli' : Node CLI module.
+     * @type {string}
+     * @default 'browser'
+     */
+    buildMode: 'node-cli',
+
+    /**
+     * Script compile configuration.
+     * @type {{}}
+     * @default {}
+     */
+    script: {
+
+      /**
+       * Script preprocessor.
+       * - 'typescript' : use typescript preprocessor to transpile source.
+       * - 'none': do not use any script preprocessor to transpile source.
+       * @type {string}
+       * @default 'none'
+       */
+      preprocessor: 'typescript',
+
+      /**
+       * Script required image compile configuration.
+       * set false if not required to inline images
+       * @type {({}|false)}
+       * @default {}
+       */
+      image: {
+
+        /**
+         * Inline image if image size is less than or equal to specified limit.
+         * @type {number}
+         * @default 1000000
+         */
+        inlineLimit: 1000000,
+
+        /**
+         * Large image output directory within distribution directory.
+         * @type {string}
+         * @default 'images'
+         */
+        outDir: 'images'
+      },
+    },
+
+    /**
+     * Style compile configuration.
+     * Set false if styles are not supported.
+     * @type {({}|false)}
+     * @default {}
+     */
+    style: false,
+
+    /**
+     * Run bundle build tasks concurrently to improve performance if true
+     * @type {boolean}
+     * @default true
+     */
+    concurrentBuild: true
+  },
+
+  /**
+   * List of paths which contains static assets referenced in style sheets.
+   * Paths should be relative to project root.
+   * @type {Array<string>}
+   * @default []
+   */
+  assetPaths: [],
+
+  /**
+   * List of files paths to copy on build.
+   * Paths should be relative to project root.
+   * @type {Array<string>}
+   * @default []
+   */
+  copy: [
+    'README.md',
+    'LICENSE',
+    "assets/**/{.*,*}"
+  ],
+
+  /**
+   * Import paths to ignore with noop implementation.
+   * Paths should be relative to project root.
+   * @type {Array<string>}
+   * @default []
+   */
+  ignore: [],
+
+  /**
+   * Import path replace pattern collection.
+   * @type {Array<{ test: (string|RegExp), replace: string }>}
+   * @default []
+   */
+  replacePatterns: [],
+
+  /**
+   * Bundle artifact build configuration.
+   * @type {{}}
+   */
+  bundle: {
+
+    /**
+     * Bundle output external dependencies (dependency modules to treat as externals).
+     * Refer rollup options for more info.
+     * @type {Array<string>}
+     * @default []
+     */
+    externals: [
+      '@babel/**',
+      'autoprefixer',
+      'chalk',
+      'glob',
+      'glob-to-regexp',
+      'gulp',
+      'gulp-chmod',
+      'gulp-clean',
+      'gulp-file',
+      'handlebars',
+      'inquirer',
+      'karma-rollup-preprocessor',
+      'less',
+      'lodash*',
+      'node-sass',
+      'plugin-error',
+      'postcss',
+      'postcss-base64',
+      'postcss-image-inliner',
+      'rollup',
+      'rollup-plugin-babel',
+      'rollup-plugin-commonjs',
+      'rollup-plugin-filesize',
+      'rollup-plugin-hbs',
+      'rollup-plugin-ignore',
+      'rollup-plugin-ignore-import',
+      'rollup-plugin-img',
+      'rollup-plugin-istanbul',
+      'rollup-plugin-livereload',
+      'rollup-plugin-node-resolve',
+      'rollup-plugin-postcss',
+      'rollup-plugin-progress',
+      'rollup-plugin-re',
+      'rollup-plugin-serve',
+      'rollup-plugin-typescript2',
+      'rollup-pluginutils',
+      'rollup-plugin-node-globals',
+      'rollup-plugin-node-builtins',
+      'rollup-plugin-json',
+      'schema-inspector',
+      'stylelint',
+      'stylus',
+      'through2',
+      'tslib',
+      'tslint',
+      'uglify-es',
+      'validate-npm-package-name',
+      'validator/**',
+      'path',
+      'fs',
+      'child_process',
+      'terser'
+    ],
+
+    /**
+     * Bundle output global dependencies (dependency modules to tread as globals).
+     * Refer rollup options for more info.
+     * @type {{}}
+     * @default {}
+     */
+    globals: {},
+
+    /**
+     * Treat globals as externals if true
+     * @type {boolean}
+     * @default true
+     */
+    mapExternals: false,
+
+    /**
+     * Browser compliant bundle modules formats (based on .babelrc.bundle.js)
+     * - 'umd' – Universal Module Definition, works as amd, cjs and iife all in one
+     * - 'amd' – Asynchronous Module Definition, used with module loaders like RequireJS
+     * - 'iife' – A self-executing function, suitable for inclusion as a DOM script tag. (If you want to create a bundle
+     * for your application, you probably want to use this, because it leads to smaller file sizes.)
+     * - 'system' - Native format of SystemJS loader
+     *
+     * NodeJS only bundle module formats
+     * 'cjs' – CommonJS, suitable for Node and Browserify/Webpack
+     * 'esm' – Keep the bundle as an ES module file
+     * @type {string}
+     * @default 'umd'
+     */
+    format: 'cjs',
+
+    /**
+     * Library global scope namespace (only applicable for browser compliant).
+     * @type {string}
+     * @default 'com.lib'
+     */
+    namespace: '',
+
+    /**
+     * AMD flat bundle configuration
+     * @type {{}}
+     * @default {}
+     */
+    amd: {
+
+      /**
+       * AMD flat bundle define function name
+       * @type {string}
+       * @default ''
+       */
+      define: '',
+
+      /**
+       * AMD flat bundle module identifier name
+       * @type {string}
+       * @default 'my-lib'
+       */
+      id: ''
+    }
+  },
+
+  /**
+   * Unit test framework
+   *  - 'jasmine'
+   *  - 'mocha'
+   *  - 'jest'
+   * @type {string}
+   * @default 'jasmine'
+   */
+  testFramework: 'jasmine',
+
+  /**
+   * Watch mode configuration
+   * Set false if not required to serve on watch build.
+   * @type {{}|false}
+   * @default {}
+   */
+  watch: false,
+
+  /**
+   * Bundle license configuration
+   * @type {{}}
+   * @default {}
+   */
+  license: {
+
+    /**
+     * Include inline header banner parsed via .packer/.banner.hbs template to build artifacts.
+     * @type {boolean}
+     * @default true
+     */
+    banner: true
+  }
+};
