f35f39421cc078d5ea22cc387bd16e0ba94c9053
Yohan Gomez <yohan.gz@hotmail.com>
2018-11-16 14:08:57 +0530

    Remove karma packer config static require

--- src/plugins/karma-packer-plugin.ts
+++ src/plugins/karma-packer-plugin.ts
@@ -3,28 +3,22 @@ import rollupPreprocessor from 'karma-rollup-preprocessor';
 import rollupIgnoreImport from 'rollup-plugin-ignore-import';
 
 import path from 'path';
-import * as inspector from 'schema-inspector';
 
 import { buildPlugin, extractBundleExternals, preBundlePlugins, resolvePlugins } from '../tasks/build-util';
 import { args } from '../tasks/util';
 import { parseScriptPreprocessorExtension } from '../tasks/parser';
-import { packerSchema } from '../tasks/validation';
 import logger from '../common/logger';
+import { meta } from '../tasks/meta';
 
 export function karmaPackerPlugin() {
   const log = logger.create('[test]');
 
   try {
     const typescript = require('typescript');
-    const packerConfig = require(path.join(process.cwd(), '.packerrc.js'));
-    const validation = inspector.validate(packerSchema, packerConfig);
-    if (!validation.valid) {
-      log.warn('malformed packer config (.packerrc):\n%s', validation.format());
-    }
+    const config = meta.readPackerConfig(log);
 
-    const config = inspector.sanitize(validation, packerConfig).data;
     const testGlob: string = path.join(config.source,
-      '**/*.spec.' + parseScriptPreprocessorExtension(config.compiler.scriptPreprocessor));
+      '**/*.spec.' + parseScriptPreprocessorExtension(config.compiler.script.preprocessor));
     log.trace('test glob: %s', testGlob);
 
     const packerPreprocess = {};
