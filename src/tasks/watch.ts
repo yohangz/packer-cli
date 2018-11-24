623f26d0ecdf4cadbe2682693de020aa110f648a
Yohan Gomez <yohan.gz@hotmail.com>
2018-11-24 07:39:11 +0530

    Watch task doc comment updates

--- src/tasks/watch.ts
+++ src/tasks/watch.ts
@@ -7,72 +7,85 @@ import rollupServe from 'rollup-plugin-serve';
 import rollupLivereload from 'rollup-plugin-livereload';
 
 import {
-  buildPlugin,
+  getScriptBuildPlugin,
   extractBundleExternals,
   getBanner,
   getBaseConfig,
-  preBundlePlugins,
-  resolvePlugins,
-  rollupStyleBuildPlugins
+  getPreBundlePlugins,
+  getDependencyResolvePlugins,
+  getStyleBuildPlugins, customRollupPlugins
 } from './build-util';
 
 import { meta } from './meta';
 import { makeRelativeDirPath } from './util';
 import logger from '../common/logger';
 
+/**
+ * Initialize source watch associated gulp tasks.
+ */
 export default function init() {
+
+  /**
+   * Watch and build source gulp task.
+   * Watch source and realtime compile when source files change.
+   * Serve and live reload demo page if bundle is browser compliant.
+   */
   gulp.task('build:watch', async () => {
     const log = logger.create('[watch]');
     try {
       const typescript = require('typescript');
-      const config = meta.readPackerConfig(log);
-      const packageJson = meta.readPackageData();
-      const banner = getBanner(config, packageJson);
-      const baseConfig = getBaseConfig(config, packageJson, banner, 'inline');
+      const packerConfig = meta.readPackerConfig(log);
+      const packageConfig = meta.readPackageData();
+      const banner = await getBanner(packerConfig, packageConfig);
+      const baseConfig = getBaseConfig(packerConfig, packageConfig, banner, 'inline');
 
-      makeRelativeDirPath(config.tmp, 'watch');
+      makeRelativeDirPath(packerConfig.tmp, 'watch');
 
       let rollupServePlugins = [];
-      if (config.watch && config.bundle.format !== 'cjs') {
+      if (packerConfig.watch && packerConfig.bundle.format !== 'cjs') {
         log.trace('build bundle with serve support');
-        const additionalServeDir = config.watch.serveDir.map((dir: string) => path.join(process.cwd(), dir));
+        const additionalServeDir = packerConfig.watch.serveDir.map((dir: string) => path.join(process.cwd(), dir));
         rollupServePlugins = [
           rollupServe({
             contentBase: [
-              path.join(process.cwd(), config.tmp, 'watch'),
-              path.join(process.cwd(), config.watch.demoDir),
-              path.join(process.cwd(), config.watch.helperDir),
+              path.join(process.cwd(), packerConfig.tmp, 'watch'),
+              path.join(process.cwd(), packerConfig.watch.demoDir),
+              path.join(process.cwd(), packerConfig.watch.helperDir),
               ...additionalServeDir
             ],
-            open: config.watch.open,
-            port: config.watch.port
+            open: packerConfig.watch.open,
+            port: packerConfig.watch.port,
+            ...packerConfig.compiler.advanced.rollup.pluginOptions.serve
           }),
           rollupLivereload({
             watch: [
-              path.join(process.cwd(), config.tmp, 'watch'),
-              path.join(process.cwd(), config.watch.demoDir)
-            ]
+              path.join(process.cwd(), packerConfig.tmp, 'watch'),
+              path.join(process.cwd(), packerConfig.watch.demoDir)
+            ],
+            ...packerConfig.compiler.advanced.rollup.pluginOptions.liveReload
           })
         ];
       } else {
         log.trace('build serve disabled or not supported for bundle type');
       }
 
-      const externals = extractBundleExternals(config);
+      const externals = extractBundleExternals(packerConfig);
+      const targetFileName = `${packageConfig.name}.${packerConfig.bundle.format}.js`;
       const watchConfig: RollupWatchOptions = merge({}, baseConfig, {
         external: externals,
         output: {
-          file: path.join(process.cwd(), config.tmp, 'watch', `${packageJson.name}.${config.bundle.format}.js`),
-          format: config.bundle.format,
-          globals: config.bundle.globals,
-          name: config.bundle.namespace
+          file: path.join(process.cwd(), packerConfig.tmp, 'watch', targetFileName),
+          format: packerConfig.bundle.format,
+          globals: packerConfig.bundle.globals,
+          name: packerConfig.bundle.namespace
         },
         plugins: [
-          ...rollupStyleBuildPlugins(config, packageJson, true, true, log),
-          ...preBundlePlugins(config),
-          ...resolvePlugins(config),
-          ...buildPlugin('bundle', false, false, config, typescript),
-          ...rollupServePlugins
+          ...getStyleBuildPlugins(packerConfig, packageConfig, true, true, log),
+          ...getPreBundlePlugins(packerConfig),
+          ...getDependencyResolvePlugins(packerConfig),
+          ...getScriptBuildPlugin('bundle', false, false, packerConfig, typescript, log),
+          ...rollupServePlugins,
+          ...customRollupPlugins(packerConfig, 'bundle')
         ],
         watch: {
           exclude: ['node_modules/**']
@@ -80,7 +93,10 @@ export default function init() {
       });
       log.trace('rollup config:\n%o', watchConfig);
 
-      const watcher = await watch([watchConfig]);
+      const watcher = await watch([ {
+        ...watchConfig,
+        ...packerConfig.compiler.advanced.rollup.watchOptions
+      }]);
       watcher.on('event', (event) => {
         switch (event.code) {
           case 'START':
@@ -103,5 +119,9 @@ export default function init() {
     }
   });
 
+  /**
+   * Watch source gulp task.
+   * Clean watch temporary files and start watch build task in series.
+   */
   gulp.task('watch', gulp.series('tmp:clean', 'build:watch'));
 }
